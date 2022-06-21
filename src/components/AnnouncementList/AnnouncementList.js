2cbc0ffaf42b66163907fbc8fca54b2a873159ae
Camilla Santos <milla.revolts@gmail.com>
2022-06-21 12:26:00 +0100

    background

---
+++ src/components/AnnouncementList/AnnouncementList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./AnnouncementList.css"
+
+
+function AnnouncementList() {
+let topic = [{
+id:"1",
+topic:"JavaScript",
+student:"Bryce",
+date:"22/06/2022",
+time:"17:30"
+},
+{
+id:"2",
+topic:"SQL",
+student:"Andy",
+date:"27/06/2022",
+time:"17:30"}]
+
+
+
+return(
+            <div className="Container-card">
+            { topic.map(function (item){
+             return(
+             <div className="Announcement-card">
+               <h3>{item.topic}</h3>
+                <p>Room: {item.id}</p>
+                <p>{item.student}</p>
+                <p>{`${item.date} ${item.time}`}</p>
+                </div>)})}      
+             </div>
+);
+    
+}
+
+export default AnnouncementList
