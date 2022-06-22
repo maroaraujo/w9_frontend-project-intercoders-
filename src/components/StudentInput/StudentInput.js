33c6a2aa84b8e478c023228e350319c8b01a3392
maroaraujo <tiagomoller@tiagos-mbp.home>
2022-06-22 11:22:55 +0100

    working the css as in figma with the columns

--- src/components/StudentInput/StudentInput.js
+++ src/components/StudentInput/StudentInput.js
@@ -1,31 +1,86 @@
 import React from "react";
-import {useState} from "react"
+import { useState } from "react";
+import axios from "axios";
 
 function StudentInput(props) {
+  /*const [studentCourse, setStudentCourse] = useState([])
 
- const [text,setText]=useState('');
- const [students, setStudent] = useState([]);
+    // To send name of students to the database
+      function postStudents() {
+        console.log("Inside Axios");
+        Axios.post("http://localhost:3001/waitinglist", {}).then((response) => {
+          console.log(response);
+          if (response.data.payload.length > 0) {
+            console.log("Hello, " + response.data.payload[0].username);
+            setStudentCourse("Hello, " + response.data.payload[0].username);
+          } else {
+            console.log("User is not found");
+            setStudentCourse("User is not found");
+          }
+        });
+      }*/
+  const [text, setText] = useState("");
+  const [students, setStudent] = useState([]);
+  const [seeMore, setSeeMore] = useState(false);
 
-  function handleChange (e) {
-    setText(e.target.value)
+  function handleChange(e) {
+    setText(e.target.value);
     // console.log(text)
-    }
+  }
 
-    function handleClick(e){
-    e.preventDefault()
-    setStudent([...students,{studentName:text.toLowerCase(), id:students.length+1,keyCourse:props.value}])
-    }
-    console.log(students)
+  function handleClick(e) {
+    e.preventDefault();
+    setStudent([
+      ...students,
+      {
+        studentName: text.toLowerCase(),
+        id: students.length + 1,
+        keyCourse: props.value,
+      },
+    ]);
+  }
+  console.log(students);
 
-return(
-   <div className='input-student'>
-        <input onChange={handleChange} type="text" placeholder="Student's name"></input>
-    <button type= "submit" onClick={handleClick}>+</button>
-     {students.map(function(students){return <li key={students.id}>{students.studentName} <button key={students.id} className='deleteButton'> X</button></li> })}
+  return (
+    <div className="input-student">
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="Student's name"
+      ></input>
+      <button type="submit" onClick={handleClick}>
+        +
+      </button>
+      <ul>
+        {!seeMore
+          ? students.slice(0, 3).map(function (students) {
+              return (
+                <li key={students.id}>
+                  •{students.studentName}{" "}
+                  <button key={students.id} className="deleteButton">
+                    {" "}
+                    X
+                  </button>
+                </li>
+              );
+            })
+          : students.map(function (students) {
+              return (
+                <li key={students.id}>
+                  •{students.studentName}{" "}
+                  <button key={students.id} className="deleteButton">
+                    {" "}
+                    X
+                  </button>
+                </li>
+              );
+            })}
+        {students.length > 4 && (
+          <button onClick={() => setSeeMore(true)}>See more</button>
+        )}
+      </ul>
     </div>
-
-)
-    
+  );
 }
 
 export default StudentInput;
--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -1,42 +1,41 @@
 import React from "react";
-import javascript from "../../img/javascript-logo.png"
-import reactlogo from "../../img/react-logo.png"
-import sql from "../../img/postgresql-logo.png"
-import node from "../../img/node.png"
+import javascript from "../../img/javascript-logo.png";
+import reactlogo from "../../img/react-logo.png";
+import sql from "../../img/postgresql-logo.png";
+import node from "../../img/node.png";
 import Volunteer from "../Volunteer/Volunter";
 import StudentInput from "../StudentInput/StudentInput";
-import "./TopicCard.css"
-
-
-
-
+import "./TopicCard.css";
 
 function TopicCard() {
-const topicCard=[
-{topicName:"JavaScript", src:javascript}, 
-{topicName:"React", src:reactlogo},
-{topicName:"SQL", src:sql},
-{topicName:"Node.JS", src:node}
-]
+  const topicCard = [
+    { topicName: "JavaScript", src: javascript },
+    { topicName: "React", src: reactlogo },
+    { topicName: "SQL", src: sql },
+    { topicName: "Node.JS", src: node },
+  ];
 
-return(
-  <div className="card--organizer">
- {topicCard.map(function(item,index) { return(
-      <div>
-    <div className='topic--categories'>
-    <img className="img" src={item.src} alt={item.topicName}/>
-    <h3>{item.topicName}</h3>
-    <div className="inputs">
-    <StudentInput value = {item.topicName.toLowerCase()} />
-    <Volunteer value={item.topicName.toLowerCase()}/>  
+  return (
+    <div className="card--organizer">
+      {topicCard.map(function (item, index) {
+        return (
+          
+            <div className="topic--categories">
+            <div className="image-course">
+              <img className="img" src={item.src} alt={item.topicName} />
+              <h3>{item.topicName}</h3>
+              </div>
+              <p className="roomnumber">Room {index+1}</p>
+              <div className="inputs">
+                <StudentInput value={item.topicName.toLowerCase()} />
+                <Volunteer value={item.topicName.toLowerCase()} />
+              </div>
+            </div>
+          
+        );
+      })}
     </div>
-    </div>
-    </div>
-    )})}
-   
- </div>
-);
-    
+  );
 }
 
 export default TopicCard;
